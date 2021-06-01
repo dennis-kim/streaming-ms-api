c968e8666a20d70db7a06c3fbfe65d4b0076684b
2020019_W1\김민섭 <minseop87@interpark.com>
2021-06-01 15:12:08 +0900

    쿼리 정리

--- service.py
+++ service.py
@@ -5,7 +5,19 @@ app = Flask (__name__)
 
 @app.route('/contents/<int:base_id>')
 def calling(base_id):
-    query = "select contents_name, modify_date from contents c inner join base_dir base ON base.base_dir_id = c.base_dir_id and base.env = 'prd' where base.base_dir_id = %s"
+    query = """
+        SELECT 
+            contents_name
+            , modify_date 
+        FROM 
+            contents c 
+                INNER JOIN base_dir base 
+                        ON base.base_dir_id = c.base_dir_id 
+                        AND base.env = 'prd' 
+        WHERE 
+            base.base_dir_id = %s
+    """
+
     value = (base_id)
     list = db.selectOneQuery(query, value)
     return 'good!!'
