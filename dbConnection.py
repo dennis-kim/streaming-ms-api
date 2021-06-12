d43af80e063577bcadc276c4665d6612cfbaf14a
김민섭 <>
2021-06-12 14:30:40 +0900

    db 접속 host 변경

--- dbConnection.py
+++ dbConnection.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def connector():
-    conn = pymysql.connect(host='118.34.135.195', port=43306, user='streaming', password='streaming12#', database='STREAMING_DB',charset='utf8')
+    conn = pymysql.connect(host='msnas.i234.me', port=43306, user='streaming', password='streaming12#', database='STREAMING_DB',charset='utf8')
     return conn
 
 def selectQuery(query):
