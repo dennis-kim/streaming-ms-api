12c953700b0d9a818a46dc52a75144887fbc60a5
김민섭 <>
2021-06-13 01:18:02 +0900

    cursorclass 추가

--- dbConnection.py
+++ dbConnection.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def connector():
-    conn = pymysql.connect(host='msnas.i234.me', port=43306, user='streaming', password='streaming12#', database='STREAMING_DB',charset='utf8')
+    conn = pymysql.connect(host='msnas.i234.me', port=43306, user='streaming', password='streaming12#', database='STREAMING_DB',charset='utf8', cursorclass=pymysql.cursors.DictCursor)
     return conn
 
 def selectQuery(query):
